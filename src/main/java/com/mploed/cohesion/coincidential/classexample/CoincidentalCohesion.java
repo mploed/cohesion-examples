9cc5242eed321cc1d45703f8e9848cc4ac4092f4
Michael Plöd <michael.ploed@gmail.com>
2024-09-27 07:53:08 +0200

    Added class cohesion example for coincidential cohesion

---
+++ src/main/java/com/mploed/cohesion/coincidential/classexample/CoincidentalCohesion.java
@@ -0,0 +1,9 @@
+package com.mploed.cohesion.coincidential.classexample;
+public class CoincidentalCohesion {
+    public void printHello() {
+        System.out.println("Hello, World!");
+    }
+    public int calculateSum(int a, int b) {
+        return a + b;
+    }
+}
